85a1d4bb228b1fc0708a6a42fca081b8731e7b64
Reynan Paiva <reynanwq@gmail.com>
2022-09-21 23:51:35 -0300

    retirando a extensao mjs e add typescript

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+//node --experimental-modules src/server.mjs
+const app = express(); //chamar a função
+app.get('/ads', (request, response) => {
+    //estamos usando o "response" do express para enviar um texto
+    return response.json([
+        { id: 1, name: "reynan" },
+        { id: 2, name: "weber" },
+        { id: 3, name: "pedro" },
+    ]); //json retorna varias coisas
+});
+app.listen(2108); //minha porta
